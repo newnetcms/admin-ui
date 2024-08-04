441ec4252886baa992d02a55f2d4c95daa69b288
KimTrien <kimtrien@gmail.com>
2024-08-04 22:44:01 +0700

    Add config credit

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -2,9 +2,7 @@
     <div class="footer-text d-flex align-items-center justify-content-between">
         <div class="copy">© {{ date('Y') }} {{ setting('site_title', 'Newnet') }}</div>
         <div class="credit">
-            <a href="https://newnetcms.com" target="_blank">
-                Newnet CMS
-            </a>
+            {!! config('core.credit') !!}
             v{{ Newnet::version() }}
         </div>
     </div>
